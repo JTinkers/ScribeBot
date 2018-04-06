acddb0e0cb338df08f4def4a1ea2e794eb212d1e
Jonek Code <jonkujanek@gmail.com>
2018-04-06 13:05:19 +0200

    Changed extensions class a little bit

--- ScribeBot/Extensions.cs
+++ ScribeBot/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,12 @@ namespace ScribeBot
     {
         public static void AppendText(this RichTextBox textBox, params object[] args )
         {
-            
+            for (int i = 0; i < args.Length; i+=2)
+            {
+                textBox.AppendText(args[i]);
+                textBox.Select(textBox.TextLength - args[i].ToString().Length, args[i].ToString().Length);
+                textBox.SelectionColor = (Color)args[i + 1];
+            }
         }
     }
 }
