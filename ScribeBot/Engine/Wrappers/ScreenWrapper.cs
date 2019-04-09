a52c4a7ddf1c776d06d6ad1336e919a1ec22fcfa
JonekCode <jonkujanek@gmail.com>
2019-04-09 19:03:47 +0200

    Fixed versioning and tabbings

--- ScribeBot/Engine/Wrappers/ScreenWrapper.cs
+++ ScribeBot/Engine/Wrappers/ScreenWrapper.cs
@@ -15,28 +15,28 @@ using ScribeBot.Engine.Containers;
 
 namespace ScribeBot.Engine.Wrappers
 {
-    /// <summary>
-    /// Wrapper containing functionality related to screen.
-    /// </summary>
-    [MoonSharpUserData]
-    static class ScreenWrapper
-    {
-        /// <summary>
-        /// Attempts to read text off of screen area using Google's Tesseract.
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="w"></param>
-        /// <param name="h"></param>
-        /// <returns>The predicted text</returns>
-        public static string Recognize(int x, int y, int w, int h)
-        {
-            var engine = new TesseractEngine(@"Library Data/Tessdata", "eng", EngineMode.Default);
+	/// <summary>
+	/// Wrapper containing functionality related to screen.
+	/// </summary>
+	[MoonSharpUserData]
+	static class ScreenWrapper
+	{
+		/// <summary>
+		/// Attempts to read text off of screen area using Google's Tesseract.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="w"></param>
+		/// <param name="h"></param>
+		/// <returns>The predicted text</returns>
+		public static string Recognize(int x, int y, int w, int h)
+		{
+			var engine = new TesseractEngine(@"Library Data/Tessdata", "eng", EngineMode.Default);
 
-            var image = Native.API.CopyScreenArea(x, y, w, h);
+			var image = Native.API.CopyScreenArea(x, y, w, h);
 
-            return engine.Process(image).GetText();
-        }
+			return engine.Process(image).GetText();
+		}
 
 		public static string Recognize(int x, int y, int w, int h, float scale = 1)
 		{
@@ -58,79 +58,79 @@ namespace ScribeBot.Engine.Wrappers
 		/// <param name="h"></param>
 		/// <returns>2D array of pixel colors.</returns>
 		public static ColorContainer[][] GetPixels(int x, int y, int w, int h)
-        {
-            Bitmap pixels = Native.API.CopyScreenArea(x, y, w, h);
+		{
+			Bitmap pixels = Native.API.CopyScreenArea(x, y, w, h);
 
-            //Has to be array of arrays rather than 2D array - silly conversion
-            ColorContainer[][] colors = new ColorContainer[w][];
-            for (int i = 0; i < w; i++)
-            {
-                colors[i] = new ColorContainer[h];
-            }
+			//Has to be array of arrays rather than 2D array - silly conversion
+			ColorContainer[][] colors = new ColorContainer[w][];
+			for (int i = 0; i < w; i++)
+			{
+				colors[i] = new ColorContainer[h];
+			}
 
-            BitmapData data = pixels.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+			BitmapData data = pixels.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
 
-            byte[] values = new byte[data.Stride * h];
+			byte[] values = new byte[data.Stride * h];
 
-            Marshal.Copy(data.Scan0, values, 0, data.Stride * h);
+			Marshal.Copy(data.Scan0, values, 0, data.Stride * h);
 
-            pixels.UnlockBits(data);
+			pixels.UnlockBits(data);
 
-            for(int j = 0; j < data.Height; j++)
-            {
-                for(int i = 0; i < data.Width; i++)
-                {
-                    colors[i][j] = new ColorContainer(values[j * data.Width * 4 + i * 4 + 2], values[j * data.Width * 4 + i * 4 + 1], values[j * data.Width * 4 + i * 4]);
-                }
-            }
+			for (int j = 0; j < data.Height; j++)
+			{
+				for (int i = 0; i < data.Width; i++)
+				{
+					colors[i][j] = new ColorContainer(values[j * data.Width * 4 + i * 4 + 2], values[j * data.Width * 4 + i * 4 + 1], values[j * data.Width * 4 + i * 4]);
+				}
+			}
 
-            return colors;
-        }
+			return colors;
+		}
 
-        /// <summary>
-        /// Take a screenshot and save it to a given path.
-        /// </summary>
-        /// <param name="path">Savepath.</param>
-        public static void Capture(string path)
-        {
-            int w, h;
+		/// <summary>
+		/// Take a screenshot and save it to a given path.
+		/// </summary>
+		/// <param name="path">Savepath.</param>
+		public static void Capture(string path)
+		{
+			int w, h;
 
-            Screen[] screens = Screen.AllScreens;
+			Screen[] screens = Screen.AllScreens;
 
-            w = screens.ToList().Sum(i => i.Bounds.Width);
-            h = screens.ToList().Max(i => i.Bounds.Height);
+			w = screens.ToList().Sum(i => i.Bounds.Width);
+			h = screens.ToList().Max(i => i.Bounds.Height);
 
-            Bitmap screen = new Bitmap( w, h, PixelFormat.Format32bppArgb );
+			Bitmap screen = new Bitmap(w, h, PixelFormat.Format32bppArgb);
 
-            Graphics gfx = Graphics.FromImage(screen);
+			Graphics gfx = Graphics.FromImage(screen);
 
-            gfx.CopyFromScreen(0, 0, 0, 0, new Size(w, h), CopyPixelOperation.SourceCopy);
+			gfx.CopyFromScreen(0, 0, 0, 0, new Size(w, h), CopyPixelOperation.SourceCopy);
 
-            try
-            {
-                if (String.IsNullOrEmpty(path))
-                    path = $@"{DateTime.Today.Day}_{DateTime.Today.Month}-{DateTime.Now.Hour}_{DateTime.Now.Minute}_{DateTime.Now.Second}.png";
+			try
+			{
+				if (String.IsNullOrEmpty(path))
+					path = $@"{DateTime.Today.Day}_{DateTime.Today.Month}-{DateTime.Now.Hour}_{DateTime.Now.Minute}_{DateTime.Now.Second}.png";
 
-                screen.Save($@"Data/User/{path}", AImageFormat.Png);
-            }
-            catch
-            {
-                Scripter.Execute($"error('Invalid path for screen.capture()! [Data/User/{path}]')");
-            }
+				screen.Save($@"Data/User/{path}", AImageFormat.Png);
+			}
+			catch
+			{
+				Scripter.Execute($"error('Invalid path for screen.capture()! [Data/User/{path}]')");
+			}
 
-            gfx.Dispose();
-            screen.Dispose();
-        }
+			gfx.Dispose();
+			screen.Dispose();
+		}
 
-        /// <summary>
-        /// Get total size of all your screens combied.
-        /// </summary>
-        /// <returns>Total size of screens.</returns>
-        public static SizeContainer GetSize()
-        {
-            Screen[] screens = Screen.AllScreens;
+		/// <summary>
+		/// Get total size of all your screens combied.
+		/// </summary>
+		/// <returns>Total size of screens.</returns>
+		public static SizeContainer GetSize()
+		{
+			Screen[] screens = Screen.AllScreens;
 
-            return new SizeContainer() { Width = screens.ToList().Sum(x => x.Bounds.Width), Height = screens.ToList().Max(i => i.Bounds.Height) };
-        }
-    }
+			return new SizeContainer() { Width = screens.ToList().Sum(x => x.Bounds.Width), Height = screens.ToList().Max(i => i.Bounds.Height) };
+		}
+	}
 }
